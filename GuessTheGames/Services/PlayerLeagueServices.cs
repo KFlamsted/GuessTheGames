d84724a8027aae77be145cbd4b711dad0bf1bf1e
KFlamsted <flamsted@gmail.com>
2016-03-24 18:39:12 +0100

    adding playerleague functions in the services, not created a controller yet

--- GuessTheGames/Services/PlayerLeagueServices.cs
+++ GuessTheGames/Services/PlayerLeagueServices.cs
@@ -24,7 +24,67 @@ namespace GuessTheGames.Services
 
         }
 
+        //get player league from id
+        public Playerleague GetLeagueFromId (int id)
+        {
+            List<Playerleague> playerleagues = new List<Playerleague>();
 
+            //getting all playerleagues in the DB
+            string sqlstring = "SELECT * FROM playerleagues WHERE id = '" + id + "';";
+            playerleagues = ReadFromDB(sqlstring);
+
+            return playerleagues[0]; //there should only be one in the list
+        }
+
+        //get one players league where he is owner
+        public List<Playerleague> GetLeagueWhereOwner(int user_id)
+        {
+            List<Playerleague> playerleagues = new List<Playerleague>();
+
+            //getting all playerleagues in the DB
+            string sqlstring = "SELECT * FROM playerleagues WHERE owner_id = '" + user_id + "';";
+            playerleagues = ReadFromDB(sqlstring);
+
+            return playerleagues;
+        }
+
+        //get all leagues in a season
+        public List<Playerleague> GetLeagueFromSeason(int season_id)
+        {
+            List<Playerleague> playerleagues = new List<Playerleague>();
+
+            //getting all playerleagues in the DB
+            string sqlstring = "SELECT * FROM playerleagues WHERE season_id = '" + season_id + "';";
+            playerleagues = ReadFromDB(sqlstring);
+
+            return playerleagues;
+        }
+        
+        //get league by name
+        public Playerleague GetLeagueFromName(string leaguename)
+        {
+            List<Playerleague> playerleagues = new List<Playerleague>();
+
+            //getting all playerleagues in the DB
+            string sqlstring = "SELECT * FROM playerleagues WHERE name = '" + leaguename + "';";
+            playerleagues = ReadFromDB(sqlstring);
+
+            return playerleagues[0]; //there should only be one in the list
+        }
+
+        //get all leagues a user is attending
+        public List<Playerleague> GetLeaguesFromUser(int user_id)
+        {
+            List<Playerleague> playerleagues = new List<Playerleague>();
+
+            //getting all playerleagues in the DB
+            string sqlstring = "SELECT * FROM playersleagues AS pl INNER JOIN user_in_playerleague AS u_in_pl ON pl.id = u_in_pl.playerleague_id WHERE user_id = '" + user_id + "' ORDER BY season_id DESC;";
+            playerleagues = ReadFromDB(sqlstring);
+
+            return playerleagues;
+        }
+        
+        
         // having a connecting to DB function with the sqlstring as input
         private List<Playerleague> ReadFromDB(string sqlstring)
         {
