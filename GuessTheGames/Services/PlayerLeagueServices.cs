4ebe2cc4483d6a84d13fb0696adc2f46f1d09594
KFlamsted <flamsted@gmail.com>
2016-03-24 14:45:21 +0100

    adding playerleagueservices and roundservices, not finished nor tested.

---
+++ GuessTheGames/Services/PlayerLeagueServices.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GuessTheGames.Models;
+using Npgsql;
+using System.Data;
+
+namespace GuessTheGames.Services
+{
+    public class PlayerLeagueServices
+    {
+
+       //get all playerleagues
+        public List<Playerleague> GetAllPlayerleagues()
+        {
+            List<Playerleague> playerleagues = new List<Playerleague>();
+
+            //getting all playerleagues in the DB
+            string sqlstring = "SELECT * FROM playerleagues;";
+            playerleagues = ReadFromDB(sqlstring);
+
+            return playerleagues; 
+
+        }
+
+
+        // having a connecting to DB function with the sqlstring as input
+        private List<Playerleague> ReadFromDB(string sqlstring)
+        {
+            List<Playerleague> playerleagues = new List<Playerleague>();
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;User Id=reader;Password=hej123;Database=guessthegame;");
+            try
+            {
+                //connecting to the database
+                conn.Open();
+
+                // defining the sql npgsql command
+                NpgsqlCommand command = new NpgsqlCommand(sqlstring, conn);
+                NpgsqlDataReader dr = command.ExecuteReader();
+
+                Playerleague tempPlayerleague;
+                User tempUser;
+                UserServices us = new UserServices();
+
+                //reading all the users.
+                while (dr.Read())
+                {
+                    tempUser = us.GetUser(dr.GetInt32(1)); //as the input into playerleague is user and not user id, we need to find the user
+                    tempPlayerleague = new Playerleague(dr.GetInt32(0), tempUser, dr.GetString(3), dr.GetInt32(2));
+                    playerleagues.Add(tempPlayerleague);
+                }
+            }
+            catch (Exception msg)
+            {
+                //do something if exception
+                System.Diagnostics.Debug.WriteLine(msg.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return playerleagues;
+        }
+    }
+}
---
+++ GuessTheGames/Services/RoundServices.cs
@@ -0,0 +1,86 @@
+﻿using GuessTheGames.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+
+namespace GuessTheGames.Services
+{
+    public class RoundServices
+    {
+
+        //get all rounds in DB
+        public List<Round> GetAllRounds()
+        {
+            List<Round> rounds = new List<Round>();
+            string sqlstring = "SELECT * FROM rounds;";
+            rounds = ReadFromDB(sqlstring);
+            return rounds;
+        }
+
+        //get the round with id in DB
+        public Round GetRoundByID(int id)
+        {
+            List<Round> rounds = new List<Round>();
+            string sqlstring = "SELECT * FROM rounds WHERE id = '" + id +"';";
+            rounds = ReadFromDB(sqlstring);
+            return rounds[0];
+        }
+
+        //get all rounds from a season
+        public List<Round> GetRoundsFromSeason(int season_id)
+        {
+            List<Round> rounds = new List<Round>();
+            string sqlstring = "SELECT * FROM rounds WHERE season_id = '" + season_id + "';";
+            rounds = ReadFromDB(sqlstring);
+            return rounds;
+        }
+
+        //get a specific round from a season using the round number
+        public Round GetRoundFromSeasonAndRound(int season_id, int round)
+        {
+            List<Round> rounds = new List<Round>();
+            string sqlstring = "SELECT * FROM rounds WHERE season_id = '" + season_id + "' AND round_number = '" + round + ";";
+            rounds = ReadFromDB(sqlstring);
+            return rounds[0];
+        }
+
+        // used for connecting to the db; to remove redundancy
+        private List<Round> ReadFromDB(string sqlstring)
+        {
+            List<Round> rounds = new List<Round>();
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;User Id=reader;Password=hej123;Database=guessthegame;");
+            try
+            {
+                //connecting to the database
+                conn.Open();
+
+                // defining the sql npgsql command
+                NpgsqlCommand command = new NpgsqlCommand(sqlstring, conn);
+                NpgsqlDataReader dr = command.ExecuteReader();
+
+                Round tempRound;
+
+                // reading all the rounds.
+                while (dr.Read())
+                {
+                    tempRound = new Round(dr.GetInt32(0), dr.GetInt32(1), dr.GetInt32(2));
+                    rounds.Add(tempRound);
+                }
+            }
+            catch (Exception msg)
+            {
+                //do something if exception
+                System.Diagnostics.Debug.WriteLine(msg.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return rounds;
+        }
+
+    }
+}
